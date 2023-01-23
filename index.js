a37b29210da708bde28509909ca5637e5fca1221
I-Slot <ismailadesholaharun@gmail.com>
2023-01-23 21:03:39 +0100

    .

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// chrome://extensions/
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
@@ -5,14 +7,21 @@ const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-    inputEl.value = "";
-    renderLeads();
+  inputEl.value = "";
+  renderLeads();
 });
 
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li><a href=' " + myLeads[i] + " '>" + myLeads[i] + "</a></li>"
+    // listItems += "<li><a target='_blank' href=' " + myLeads[i] + " '>" + myLeads[i] + "</a></li>"
+    listItems += `
+    <li>
+        <a target='_blank' href='${myLeads[i]}'>
+            ${myLeads[i]}
+        </a>
+    </li>
+`;
     //  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
     // create element
     // set text content
