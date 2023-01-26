b5a39b21a6f10c15902e76672b3a2dc2db67fa52
I-Slot <ismailadesholaharun@gmail.com>
2023-01-26 21:47:08 +0100

    .

--- index.js
+++ index.js
@@ -1,48 +1,44 @@
+let myLeads = []
+let oldLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+// 1. Store the delete button in a deleteBtn variable
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
-// chrome://extensions/
-let myLeads = [];
-const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById("input-btn");
-const ulEl = document.getElementById("ul-el");
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
-// let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads" , JSON.parse(myLeads) ));
-console.log(leadsFromLocalStorage)
-
-
-// localStorage.setItem("MyName", "Ismail Adeshola Ogundele");
-let myName = localStorage.getItem("myName");
-console.log(myName)
 if (leadsFromLocalStorage) {
-    myName = leadsFromLocalStorage
-    renderLeads()
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
 }
 
-
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-  renderLeads();
-});
-
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    // listItems += "<li><a target='_blank' href=' " + myLeads[i] + " '>" + myLeads[i] + "</a></li>"
-    listItems += `
-    <li>
-        <a target='_blank' href='${myLeads[i]}'>
-            ${myLeads[i]}
-        </a>
-    </li>
-`;
-    //  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-    // create element
-    // set text content
-    // append to ul
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
+function render(leads) {
+  let listItems = ""
+  for (let i = 0; i < leads.length; i++) {
+      listItems += `
+          <li>
+              <a target='_blank' href='${leads[i]}'>
+                  ${leads[i]}
+              </a>
+          </li>
+      `
   }
-  ulEl.innerHTML = listItems;
+  ulEl.innerHTML = listItems  
 }
+
+
+// 2. Listen for double clicks on the delete button (google it!)
+// 3. When clicked, clear localStorage, myLeads, and the DOM
+
+deleteBtn.addEventListener("dblclick", function() {
+  localStorage.clear()
+  myLeads = []
+  render(myLeads)
+})
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+    render(myLeads)
+})
